41573c045a6ac1ba8bfe153b6c69c4b558bdc482
bobruis <bob.r.ruis@gmail.com>
2018-03-13 21:24:51 +0100

    Update files
    Changing CSS, added scripts to javascript. displays goodmorning and good evening. Added some html tags in index.html

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,6 +4,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 	var currentTime = document.getElementById('current-time'),
 		currentDate = document.getElementById('current-date');
+
 	
 	setInterval(function() {
 		var d = new Date();
@@ -11,26 +12,29 @@ document.addEventListener('DOMContentLoaded', function(){
 		var hours = d.getHours(),
 			minutes = d.getMinutes(),
 			month = formatMonth(d.getMonth()),
-			date = d.getDate(),
-			ampm = 'AM';
-			
-		if (hours > 12) {
-			hours -= 12;
-			ampm = 'PM';
-		} else if (hours === 0) {
-			hours = 12;
-		}
-		
+			date = d.getDate();
+					
 		if (minutes < 10) {
 			minutes = '0' + minutes;
 		}
+
+		var message = '';
+
+			if(hours >= 18){
+				message = 'Goedeavond citizen';
+			}
+			else{
+				message = 'Goedemiddag citizen';
+			}
+		document.getElementById('welcome-message').innerHTML = message;
 		
 		var sepClass = '';
-		if (d.getSeconds() % 2 === 1) sepClass = 'trans';
+			
+			if (d.getSeconds() % 2 === 1) sepClass = 'trans';
 		
 		var sep = '<span class="' + sepClass + '">:</span>';
 	
-		currentTime.innerHTML = hours + sep + minutes + ' ' + ampm;
+		currentTime.innerHTML = hours + sep + minutes;
 		currentDate.textContent = month + ' ' + date;
 	}, 1000);
 	
@@ -42,30 +46,25 @@ document.addEventListener('DOMContentLoaded', function(){
 		} else if (m > 11) {
 			m = 11;
 		}
-		
-		/*
-		var monthName;
-		switch(m) {
-			case 0 :
-				monthName = "January";
-				break;
-			case 1 :
-				monthName = "February";
-				break;
-			// and so on...
-		}
-		*/
 	
 		var monthNames = [
-			'January', 'February', 'March',
-			'April', 'May', 'June', 
-			'July', 'August', 'September',
-			'October', 'November', 'December'
+			'januari', 'februari', 'maart',
+			'april', 'mei', 'juni', 
+			'juli', 'augustus', 'september',
+			'oktober', 'november', 'december'
 		];
 		
 		return monthNames[m];
+
+	// functie om de juiste maand te pakken
+	// controleert of er niet iets onder de 0 of boven de 11 aanwezig is
+	// var montNames is een array die start bij 0 daardoor heeft [m] bij 0 - january etc. 
 	}
+
+
+
 });
 
+	
 
 })();
